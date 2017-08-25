943c4b7c34451e4d376c25c2d1a24f9beac32c42
glowychan <glochan.uog@gmail.com>
2017-08-25 15:02:38 -0400

    create server and ejs template

---
+++ express_server.js
@@ -0,0 +1,23 @@
+var express = require("express");
+var app = express();
+var PORT = process.env.PORT || 8080; // default port 8080
+
+var urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
+app.get("/", (req, res) => {
+  res.end("Hello!");
+});
+
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
+
+app.set("view engine", "ejs");
